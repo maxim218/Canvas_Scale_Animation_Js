a5a2b3b5fad074d755ffdb39fd36b15bdbdfbbab
maxim <maxim218maxim218@yandex.ru>
2018-04-25 22:33:49 +0300

    Add fullscreen API mode. Stop copy

--- static/scripts/Start.js
+++ static/scripts/Start.js
@@ -8,34 +8,15 @@ class Start {
     constructor() {
         this.canvasManager = new CanvasManager();
 
-        this.initImage();
-
-        window.onresize = () => {
-            this.resizeImage();
-        };
-
-        this.resizeImage();
+        document.getElementById("btn").onclick = function() {
+            document.getElementById('can').webkitRequestFullscreen();
+        }
 
         let interval = setInterval(() => {
             this.canvasManager.moveLeft();
             this.canvasManager.drawFon();
             this.canvasManager.drawRectangle();
-            this.cloneImage();
         }, WAIT_TIME);
-    }
-
-    initImage() {
-        this.image = document.getElementById("renderImage");
-        this.image.hidden = false;
-    }
-
-    cloneImage() {
-        this.image.src = this.canvasManager.getCanvas().toDataURL("image/png");
-    }
-
-    resizeImage() {
-        this.image.width = document.documentElement.clientWidth;
-        this.image.height = document.documentElement.clientHeight;
     }
 }
 
